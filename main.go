7df143c40c521cc6c3c382d7ff79dfa6ab5e3ad2
vahe.muradyan.03 <vahe.muradyan.03@gamil.com>
2025-04-18 16:52:22 +0400

    meki 2-rd prca

--- main.go
+++ main.go
@@ -1,19 +1,38 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"log"
 	"os"
+	"strconv"
 )
 
 func main() {
 	filePath := "example.txt"
+	totalSum := 0
+	totalCount := 0
 
-	content, err := os.ReadFile(filePath)
+	file, err := os.Open(filePath)
 	if err != nil {
 		log.Fatal(err)
 	}
+	defer file.Close()
 
-	fmt.Println("Full file content:")
-	fmt.Println(string(content))
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		val, _ := strconv.Atoi(line)
+		totalCount++
+		totalSum += val
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	if totalCount > 0 {
+		fmt.Println(totalSum / totalCount)
+	}
 }
