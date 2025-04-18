62005b68d365d7b1c9d44f890cc169efd92e2828
vahe.muradyan.03 <vahe.muradyan.03@gamil.com>
2025-04-18 17:33:16 +0400

    1-in masin 3-rd das@ prca

--- main.go
+++ main.go
@@ -35,4 +35,52 @@ func main() {
 	if totalCount > 0 {
 		fmt.Println(totalSum / totalCount)
 	}
+
+	ReadRuny(filePath)
+}
+
+func ReadRuny(filePath string) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	reader := bufio.NewReader(file)
+
+	charactersToRead := 100
+
+	for charactersToRead > 0 {
+		char, _, err := reader.ReadRune()
+		if err != nil {
+			break
+		}
+
+		fmt.Println(string(char))
+		charactersToRead--
+	}
+
+	fmt.Println("...")
+}
+
+func ReadLineAndRuny(filePath string) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		fmt.Println(line)
+
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	file.Seek(0, 0) // senc pointer@ hetem berum amenaskizb
 }
